c606ffa0a433f61c4f35c63c176850c08ff29aca
Kenneth Mandawe <kenneth.mandawe@cheetahdigital.com>
2020-04-07 18:57:45 +0800

    27. Multiple Decorators

---
+++ multiple_decorators.py
@@ -0,0 +1,39 @@
+def escape_unicode(f):
+    def wrap(*args, **kwargs):
+        x = f(*args, **kwargs)
+        return ascii(x)
+
+    return wrap
+
+
+class Trace:
+    def __init__(self):
+        self.enabled = True
+
+    def __call__(self, f):
+        def wrap(*args, **kwargs):
+            if self.enabled:
+                print("Calling {}".format(f))
+            return f(*args, **kwargs)
+
+        return wrap
+
+
+tracer = Trace()
+
+
+@tracer
+@escape_unicode
+def norweigian_island_maker(name):
+    return name + "ðy"
+
+
+print(norweigian_island_maker("Llama"))
+print(norweigian_island_maker("Python"))
+print(norweigian_island_maker("Troll"))
+
+tracer.enabled = False
+
+print(norweigian_island_maker("Llama"))
+print(norweigian_island_maker("Python"))
+print(norweigian_island_maker("Troll"))
