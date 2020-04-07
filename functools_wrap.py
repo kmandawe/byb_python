abca51d263d9b513a8297ea5296d6c8b54f8da84
Kenneth Mandawe <kenneth.mandawe@cheetahdigital.com>
2020-04-07 19:48:08 +0800

    29. functools.wraps()

---
+++ functools_wrap.py
@@ -0,0 +1,21 @@
+import functools
+
+
+def noop(f):
+    @functools.wraps(f)
+    def noop_wrapper():
+        return f()
+
+    return noop_wrapper
+
+
+@noop
+def hello():
+    "Print a well-known message."
+    print("Hello, world!")
+
+
+print(hello.__name__)
+print(hello.__doc__)
+
+help(hello)
