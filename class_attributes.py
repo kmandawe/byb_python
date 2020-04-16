dc3d37b8ec8d083a64be60b445d3a58754ae9b33
Kenneth Mandawe <kenneth.mandawe@cheetahdigital.com>
2020-04-16 22:10:42 +0800

    36. Template Method Pattern

--- class_attributes.py
+++ class_attributes.py
@@ -32,6 +32,9 @@ class ShippingContainer:
 
     @property
     def volume_ft3(self):
+        return self._calc_volume()
+
+    def _calc_volume(self):
         return ShippingContainer.HEIGHT_FT * ShippingContainer.WIDTH_FT * self.length_ft
 
 
@@ -93,6 +96,9 @@ class RefrigeratedShippingContainer(ShippingContainer):
 
     @celsius.setter
     def celsius(self, value):
+        self._set_celsius(value)
+
+    def _set_celsius(self, value):
         if value > RefrigeratedShippingContainer.MAX_CELSIUS:
             raise ValueError("Temperature too hot!")
         self._celsius = value
@@ -105,9 +111,8 @@ class RefrigeratedShippingContainer(ShippingContainer):
     def fahrenheit(self, value):
         self.celsius = RefrigeratedShippingContainer._f_to_c(value)
 
-    @property
-    def volume_ft3(self):
-        return super().volume_ft3 - RefrigeratedShippingContainer.FRIDGE_VOLUME_FT3
+    def _calc_volume(self):
+        return super()._calc_volume() - RefrigeratedShippingContainer.FRIDGE_VOLUME_FT3
 
 
 # r1 = RefrigeratedShippingContainer("MAE", 'fish')
@@ -171,12 +176,10 @@ print(r.volume_ft3)
 class HeatedRefrigeratedShippinContainer(RefrigeratedShippingContainer):
     MIN_CELSIUS = -20.0
 
-    @RefrigeratedShippingContainer.celsius.setter
-    def celsius(self, value):
+    def _set_celsius(self, value):
         if value < HeatedRefrigeratedShippinContainer.MIN_CELSIUS:
             raise ValueError("Temperature too cold!")
-        # super().celsius = value
-        RefrigeratedShippingContainer.celsius.fset(self, value)
+        super()._set_celsius(value)
 
 
 h1 = HeatedRefrigeratedShippinContainer.create_empty("YML", length_ft=40, celsius=-18.0)
