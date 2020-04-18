1b3cd00282d7ed928ef3c7a0f337135131b38237
Kenneth Mandawe <kenneth.mandawe@cheetahdigital.com>
2020-04-18 18:34:18 +0800

    38. str()

--- string_representations.py
+++ string_representations.py
@@ -6,11 +6,16 @@ class Point2D:
     def __str__(self):
         return '({}, {})'.format(self.x, self.y)
 
-    # def __repr__(self):
-    #     return 'Point2D(x={}, y={})'.format(self.x, self.y)
+    def __repr__(self):
+        return 'Point2D(x={}, y={})'.format(self.x, self.y)
 
 
 p = Point2D(x=42, y=69)
 print(p)
 print(str(p))
 print(repr(p))
+
+p = Point2D(123, 456)
+print(str(p))
+print("The circle is centered at {}.".format(p))
+print("The circle is centered at {}.".format(repr(p)))
