f641319f5fffe302b1a201df8697eaa90ff127d9
Kenneth Mandawe <kenneth.mandawe@cheetahdigital.com>
2020-04-18 18:41:59 +0800

    39. When are representations used

--- string_representations.py
+++ string_representations.py
@@ -10,12 +10,24 @@ class Point2D:
         return 'Point2D(x={}, y={})'.format(self.x, self.y)
 
 
-p = Point2D(x=42, y=69)
-print(p)
-print(str(p))
-print(repr(p))
+# p = Point2D(x=42, y=69)
+# print(p)
+# print(str(p))
+# print(repr(p))
+#
+# p = Point2D(123, 456)
+# print(str(p))
+# print("The circle is centered at {}.".format(p))
+# print("The circle is centered at {}.".format(repr(p)))
+#
+# print(Point2D(3, 5))
 
-p = Point2D(123, 456)
-print(str(p))
-print("The circle is centered at {}.".format(p))
-print("The circle is centered at {}.".format(repr(p)))
+l = [Point2D(i, i * 2) for i in range(3)]
+print(str(l))
+print(repr(l))
+
+d = {i: Point2D(i, i * 2) for i in range(3)}
+print(str(d))
+print(repr(d))
+
+
