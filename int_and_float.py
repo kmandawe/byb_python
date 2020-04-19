b4a47d965a4fc7763fc57e20cb01bd090925933b
Kenneth Mandawe <kenneth.mandawe@cheetahdigital.com>
2020-04-19 22:01:57 +0800

    43. Reviewing int and float

---
+++ int_and_float.py
@@ -0,0 +1,24 @@
+from math import factorial as fac
+
+print(fac(1000))
+
+import sys
+print(sys.float_info)
+
+most_negative_float = -sys.float_info.max
+print(most_negative_float)
+
+greatest_negative_float = -sys.float_info.min
+print(greatest_negative_float)
+
+print(float(10))
+
+print(2**53)
+print(float(2**53))
+print(float(2**53 + 1))
+print(float(2**53 + 2))
+print(float(2**53 + 3))
+print(float(2**53 + 4))
+
+print(0.8 - 0.7)
+print(2 / 3)
