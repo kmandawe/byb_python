babdda66cd7433c00edf2c7ce3164ed8170a0c20
Kenneth Mandawe <kenneth.mandawe@cheetahdigital.com>
2020-04-20 11:37:58 +0800

    48. Number Base Conversions

---
+++ base_conversions.py
@@ -0,0 +1,14 @@
+print(0b101010)
+print(0o52)
+print(0x2a)
+
+print(bin(42))
+print(oct(42))
+print(hex(42))
+
+print(hex(42)[2:])
+
+print(int("2a", base=16))
+print(int("acghd", base=18))
+print(int("0b101010", base=2))
+print(int("0o664", base=0))
