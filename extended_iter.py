67fa8298457c12b9b037b120f96b55826d8dff69
Kenneth Mandawe <kenneth.mandawe@cheetahdigital.com>
2020-04-23 18:32:13 +0800

    66.Extended iter() format

---
+++ extended_iter.py
@@ -0,0 +1,14 @@
+import datetime
+
+i = iter(datetime.datetime.now, None)
+print(next(i))
+print(next(i))
+print(next(i))
+
+
+with open('ending_file.txt', 'rt') as f:
+    for line in iter(lambda: f.readline().strip(), 'END'):
+        print(line)
+
+
+
