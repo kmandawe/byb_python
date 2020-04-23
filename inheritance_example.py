e03fe9b206e35ee81a98a0e93a7f735324c16233
Kenneth Mandawe <kenneth.mandawe@cheetahdigital.com>
2020-04-23 20:07:12 +0800

    69. Realistic Example: SortedList

---
+++ inheritance_example.py
@@ -0,0 +1,41 @@
+class SimpleList:
+    def __init__(self, items):
+        self._items = list(items)
+
+    def add(self, item):
+        self._items.append(item)
+
+    def __getitem__(self, item):
+        return self._items[item]
+
+    def sort(self):
+        self._items.sort()
+
+    def __len__(self):
+        return len(self._items)
+
+    def __repr__(self):
+        return "SimpleList({!r})".format(self._items)
+
+
+class SortedList(SimpleList):
+    def __init__(self, items=()):
+        super().__init__(items)
+        self.sort()
+
+    def add(self, item):
+        super().add(item)
+        self.sort()
+
+    def __repr__(self):
+        return "SortedList({!r})".format(list(self))
+
+
+sl = SortedList([4, 3, 78, 11])
+
+print(sl)
+print(len(sl))
+sl.add(-42)
+print(sl)
+sl.add(7)
+print(sl)
