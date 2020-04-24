678ec08be973b4a23514786105a76e015f7d9d8c
Kenneth Mandawe <kenneth.mandawe@cheetahdigital.com>
2020-04-24 14:34:13 +0800

    73. Details of Multiple Inheritance

--- multiple_inheritance.py
+++ multiple_inheritance.py
@@ -1,4 +1,4 @@
-from inheritance_example import SortedIntList
+from inheritance_example import SortedIntList, IntList
 
 sil = SortedIntList([42, 23, 2])
 print(sil)
@@ -8,3 +8,24 @@ print(sil)
 sil.add(-1234)
 print(sil)
 # sil.add('the smallest uninteresting number')
+
+
+class Base1:
+    def __init__(self):
+        print('Base1.__init__')
+
+
+class Base2:
+    def __init__(self):
+        print('Base2.__init__')
+
+
+class Sub(Base1, Base2):
+    pass
+
+
+s = Sub()
+
+print(SortedIntList.__bases__)
+
+print(IntList.__bases__)
