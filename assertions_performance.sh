9ccc9baac5c229276430588ae0cc1e5192cd5a9b
Kenneth Mandawe <kenneth.mandawe@cheetahdigital.com>
2020-04-27 12:16:21 +0800

    102. Assertions: Performance

---
+++ assertions_performance.sh
@@ -0,0 +1,1 @@
+python3 -O -m timeit -n 1 -s "from random import randrange; from sorted_set import SortedSet; s = SortedSet(randrange(1000) for _ in range(2000))" "[s.count(i) for i in range(1000)]"
