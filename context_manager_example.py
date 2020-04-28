c7c258972faec60d7c1148bbea62d2093ba881a7
Kenneth Mandawe <kenneth.mandawe@cheetahdigital.com>
2020-04-28 09:45:04 +0800

    105. A First Context Manager Example

--- context_manager_example.py
+++ context_manager_example.py
@@ -1,3 +1,19 @@
 with open('important_data.txt', 'w') as f:
     f.write('The secret password is 12345')
 
+
+class LoggingContextManager:
+
+    def __enter__(self):
+        print('LoggingContextManager.__enter__()')
+        return 'You are in a with-block!'
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        print('LoggingContextManager.__exit__({}, {}, {})'.format(exc_type, exc_val, exc_tb))
+        return
+
+
+with LoggingContextManager() as x:
+    raise ValueError('Something has gone wrong!')
+    print(x)
+
