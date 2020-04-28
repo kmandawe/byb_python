739ea68487ab9daf78a70f3018f28daddbed2c1a
Kenneth Mandawe <kenneth.mandawe@cheetahdigital.com>
2020-04-28 10:19:09 +0800

    109. contextlib.contextmanager

--- context_manager_example.py
+++ context_manager_example.py
@@ -1,3 +1,6 @@
+import contextlib
+import sys
+
 with open('important_data.txt', 'w') as f:
     f.write('The secret password is 12345')
 
@@ -39,3 +42,22 @@ try:
         raise ValueError("The system is down!")
 except ValueError:
     print("*** ValueError detected ***")
+
+
+@contextlib.contextmanager
+def logging_context_manager():
+    print("logging_context_manager: enter")
+    try:
+        yield 'You are in a with-block!'
+        print('logging_context_manager: normal exit')
+    except Exception:
+        print('logging_context_manager: exceptional exit', sys.exc_info())
+        raise
+
+
+with logging_context_manager() as x:
+    print(x)
+
+
+with logging_context_manager() as x:
+    raise ValueError("Something went wrong!")
