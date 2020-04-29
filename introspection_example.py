6d52805b0a538581c1f85ac9ef63460236f23770
Kenneth Mandawe <kenneth.mandawe@cheetahdigital.com>
2020-04-29 10:03:01 +0800

    112. Object Types in Depth

---
+++ introspection_example.py
@@ -0,0 +1,13 @@
+i = 7
+print(type(i))
+print(int)
+print(repr(int))
+print(type(i) is int)
+print(type(i)(78))
+print(type(type(int)))
+print(i.__class__)
+print(i.__class__.__class__)
+print(i.__class__.__class__.__class__)
+print(issubclass(type, object))
+print(type(object))
+print(isinstance(i, int))
